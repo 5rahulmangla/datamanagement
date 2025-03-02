f919fd59c1ca550be0931471e3a72bfe367e894e
5rahulmangla <5rahulmangla@gmail.com>
2025-03-02 16:26:12 +0530

    first commit.

---
+++ src/main/java/com/dm/datamanagement/dataManagement/DataManagementApplication.java
@@ -0,0 +1,14 @@
+package com.dm.datamanagement.dataManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataManagementApplication.class, args);
+	}
+		/*System.out.println("testing the data management###");*/
+	  /*System.out.println("testing the app");*/
+}
---
+++ src/test/java/com/dm/datamanagement/dataManagement/DataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.datamanagement.dataManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
